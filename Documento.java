8b35f2f27522b6cb6809ba85ddce032fefd1e31b
jaimearroyo7 <jaime.arroyo@est.fib.upc.edu>
2016-10-26 16:34:45 +0200

    Add files via upload

---
+++ Documento.java
@@ -0,0 +1,36 @@
+
+public class Documento {
+	private String titulo;
+	private String autor;
+	private String categoria;
+	private Texto texto;
+	
+	public Documento(){		
+	}
+	
+	public void setTitulo(String valortitulo){
+		titulo = valortitulo;
+	}
+	
+	public void setAutor(String valorautor){
+		autor = valorautor;
+	}
+	
+	public void setCategoria(String valorcategoria){
+		categoria = valorcategoria;
+	}
+	
+	public void setTexto(Texto valorTexto){
+		texto = valorTexto;
+	}
+	
+	public String getTitulo() { return titulo; }
+	
+	public String getAutor() { return autor; }
+	
+	public String getCategoria() { return categoria; }
+	
+	public Texto getTexto() { return texto; }
+	
+	
+}
---
+++ Texto.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class Texto {
+	private List texto;
+	
+	public Texto(){
+		
+	}
+	
+	public void setTexto(List valortexto){
+		texto = valortexto;
+	}
+	
+	public List getTexto() { return texto; }
+}
