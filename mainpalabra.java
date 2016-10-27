de918a60375cbf309502c06f12f615b9e3ff9a0e
raulva5 <raulva18@gmail.com>
2016-10-27 17:15:31 +0200

    Add files via upload

---
+++ mainpalabra.java
@@ -0,0 +1,59 @@
+package Dominio;
+import java.io.*;
+
+public class mainpalabra {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Palabra p = new Palabra("hola");
+		Palabra p2 = new Palabra();
+		
+		p2.setPalabra("adios");
+		
+		System.out.println(p.getPalabra());
+		System.out.println(p2.getPalabra());
+		
+		
+		
+		/*---------------------------------------------------------------------------------
+		
+		 prueba de escritura de las palabrasfuncionales importadas de  .txt
+		
+		---------------------------------------------------------------------------------*/
+		
+		File archivo = null; // para abrir archivo
+	    FileReader fr = null; // para leer archivos
+	    BufferedReader br = null; // lee texto de una entrada de caracteres
+	    
+	    try {
+	         archivo = new File ("C:/Users/JMC/workspace/Gestor de documentos/src/Dominio/PalabrasFuncionales");
+	         fr = new FileReader (archivo);
+	         br = new BufferedReader(fr);
+
+	         // Lectura del fichero
+	         String linea;
+	         while((linea=br.readLine())!=null) {	        	
+	            System.out.println(linea);
+	    	 }
+	    }
+	    
+	    catch(Exception e){
+	         e.printStackTrace();
+	    }
+	    
+	    finally {
+	         // En el finally cerramos el fichero, para asegurarnos
+	         // que se cierra tanto si todo va bien como si salta 
+	         // una excepcion.
+	         try{                    
+	            if( null != fr ){   
+	               fr.close();     
+	            }                  
+	         }
+	         catch (Exception e2){ 
+	            e2.printStackTrace();
+	         }
+	    }
+	}
+}
