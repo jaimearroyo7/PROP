6e474e5c5ad0cf8ca815bfe3aed35ff674684ac3
jaimearroyo7 <jaime.arroyo@est.fib.upc.edu>
2016-10-28 15:49:50 +0200

    Add files via upload

--- Pair.java
+++ Pair.java
@@ -1,12 +1,55 @@
-/**
- * Created by mhitz on 27/10/16.
- */
-public class Pair <T1, T2> {
-    public T1 first;
-    public T2 second;
 
-    public Pair(T1 first, T2 second) {
-        this.first = first;
-        this.second = second;
-    }
-}
+public class Pair<A, B> {
+	 private A first;
+	 private B second;
+	 
+	 public Pair(){}
+	 
+	 public Pair(A first, B second) {
+	 	super();
+	 	this.first = first;
+	   	this.second = second;
+	 }
+
+	 public int hashCode() {
+		 int hashFirst = first != null ? first.hashCode() : 0;
+		 int hashSecond = second != null ? second.hashCode() : 0;
+		 return (hashFirst + hashSecond) * hashSecond + hashFirst;
+	 }
+	 
+	 public boolean equals(Object other) {
+	    	if (other instanceof Pair) {
+	    		Pair otherPair = (Pair) other;
+	    		return 
+	    		((  this.first == otherPair.first ||
+	    			( this.first != null && otherPair.first != null &&
+	    			  this.first.equals(otherPair.first))) &&
+	    		 (	this.second == otherPair.second ||
+	    			( this.second != null && otherPair.second != null &&
+	    			  this.second.equals(otherPair.second))) );
+	    	}
+
+	    	return false;
+	  }
+
+	  public String toString(){ 
+	           return "(" + first + ", " + second + ")"; 
+	    }
+	  
+	  public A first() {
+	    	return first;
+	    }
+
+	  public void setFirst(A first) {
+	    	this.first = first;
+	  }
+
+	  public B second() {
+	    	return second;
+	  }
+
+   public void setSecond(B second) {
+	    	this.second = second;
+	  }
+ }
+
