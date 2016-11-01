61814a8b38e7e1b4b352e82e6a47bf5b68dbaa30
LBrull <lau.p.g15@gmail.com>
2016-11-01 14:34:35 +0100

    Add files via upload

--- ExpBool.java
+++ ExpBool.java
@@ -3,13 +3,14 @@ import java.util.*;
 
 public class ExpBool {
 	private String s;
+	private ArrayList<String> elements = new ArrayList<String>();
 	private Nodo raiz;	//tree es el nodo raíz del árbol que almacena la expresión booleana.
 	
 	//constructoras
 	public ExpBool() {}
 	public ExpBool(String s) {
 		this.s = s;
-		buildTree(this.s);
+		//buildTree(this.s);
 	}
 	
 	//getters
@@ -17,13 +18,82 @@ public class ExpBool {
 		return this.s;
 	}
 	
+	public ArrayList<String> getElements() {
+		return elements;
+	}
+	
 	//setters
 	public void setExpresio(String s) {
 		this.s = s;
-		buildTree(this.s);
+		//buildTree(this.s);
 	}
 	
-	public void buildTree (String s) {
-		
+	/*public void buildTree (String s) {
+		ArrayList<String> a = separaElements();
+		Stack<ExpBool> sexp = new Stack<ExpBool>();
+		Stack<Operador> sop = new Stack<Operador>();
+		for (int i=0; i<s.length(); ++i) {
+		}
+	}*/
+	public void separaElements() {
+		String s = new String();
+		boolean dinsdecomilles=false;
+		for (int i=0; i<this.s.length(); ++i) {
+			//si em trobo lletres:
+			if ((this.s.charAt(i) >= 65 && this.s.charAt(i) <= 90) || (this.s.charAt(i) >= 97 && this.s.charAt(i) <= 122) || 
+				(this.s.charAt(i) == 129) || (this.s.charAt(i) == 130) || 
+				(this.s.charAt(i) == 144) || (this.s.charAt(i) == 181) || 
+				(this.s.charAt(i) == 214) || (this.s.charAt(i) == 224) || 
+				(this.s.charAt(i) == 233) || (this.s.charAt(i) == 'á') ||
+				(this.s.charAt(i) == 'í') || (this.s.charAt(i) == 'ó') ||
+				(this.s.charAt(i) == 'ú') || (this.s.charAt(i) == 'ñ') ||
+				(this.s.charAt(i) == 'Á') || (this.s.charAt(i) == 'Í') ||
+				(this.s.charAt(i) == 'Ó') || (this.s.charAt(i) == 'Ú') ||
+				(this.s.charAt(i) == 'Ñ')) {
+				
+				if (i == this.s.length()-1) {
+					s = s + this.s.charAt(i);
+					elements.add(s);
+				}
+				else {
+					s = s + this.s.charAt(i);
+				}
+			}
+			//si em trobo altres caràcters:
+			if (this.s.charAt(i) == ' ' && !dinsdecomilles) {
+				elements.add(s);
+				s="";
+			}
+			else if (this.s.charAt(i) == ' ' && dinsdecomilles) {
+				s = s + this.s.charAt(i);
+			}
+			else if (this.s.charAt(i) == '"' && !dinsdecomilles) {
+				dinsdecomilles = true;
+			}
+			else if (this.s.charAt(i) == '"' && dinsdecomilles) {
+				dinsdecomilles = false;
+			}
+			else if (this.s.charAt(i) == '(') {
+				s="(";
+				elements.add(s);
+				s="";
+			}
+			else if (this.s.charAt(i) == ')') {
+				elements.add(s);
+				s=")";
+				elements.add(s);
+				s="";
+			}
+			else if (this.s.charAt(i) == '\n') {
+				elements.add(s);
+				s="";
+			}
+		}
+		//elimino espais en blanc sobrants
+		for (int i=0; i<elements.size(); ++i) {
+			if (elements.get(i).equals("")) {
+				elements.remove(i);
+			}
+		}
 	}
 }
---
+++ mainLauraExpBool.java
@@ -0,0 +1,18 @@
+package Dominio;
+import java.util.*;
+
+public class mainLauraExpBool {
+
+	public static void main(String[] args) {
+		Scanner capt = new Scanner(System.in);
+		System.out.print("Ingrese la expresion: ");
+		ExpBool exp = new ExpBool(capt.nextLine());
+		exp.separaElements();
+		ArrayList<String> a =exp.getElements();
+		for (int i=0; i<a.size(); ++i) {
+			System.out.print(a.get(i));
+			System.out.print("\n");
+		}
+	}
+
+}
