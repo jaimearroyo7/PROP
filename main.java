50cb73c0b15208b574dafba63bbf6d36f6a9a98c
raulva5 <raulva18@gmail.com>
2016-11-04 12:43:52 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,18 @@
+package dominio;
+import java.io.FileNotFoundException;
+import java.text.ParseException;
+
+public class main {
+
+	public static void main(String[] args) throws FileNotFoundException, ParseException {
+		// TODO Auto-generated method stub
+		Documentos d = new Documentos();
+		d.addDocvers1();
+		/*d.addDoc("C:/Users/JMC/workspace/Gestor de documentos/src/Dominio/prueba");*/
+		d.consultarTitulo(0);
+		d.consultarAutor(0);
+		d.consultarCategoria(0);
+		//d.consultarFecha(0);
+		d.consultarTexto(0);
+		}
+}
