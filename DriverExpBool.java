e799f360c541097a608c23d0fece59644cec43f7
LBrull <lau.p.g15@gmail.com>
2016-11-10 18:54:57 +0100

    Add files via upload

--- DriverExpBool.java
+++ DriverExpBool.java
@@ -6,29 +6,29 @@ public class DriverExpBool {
 	public static void main(String[] args) {
 		Scanner capt = new Scanner(System.in);
 		System.out.print("Ingrese la expresion: ");
-		ExpBool exp = new ExpBool();
+		ExpBool exp = new ExpBool(); //llamada a la constructora
 		int error;
 		error = exp.setExpresio(capt.nextLine()); //llamada a setExpresio
-		if (error == 0) {
+		if (error == 0) { //si no hay error en la expresion
 			ArrayList<Element> a = exp.getElements(); //llamada a getElements
 			System.out.print("Expresion en notacion prefija: \n");
 			for (int i=0; i<a.size(); ++i) {
 				System.out.print(a.get(i).getElement());
 				System.out.print("\n");
 			}
-			System.out.print("Recorrido preorden: ");
+			System.out.print("Recorrido preorden: "); //llamada a getPreorden
 			ArrayList<String> pre = exp.getPreorden();
 			for (int i = 0; i<pre.size(); ++i) {
 				System.out.print(pre.get(i));
 				System.out.print("\n");
 			}
-			System.out.print("Recorrido inorden: ");
+			System.out.print("Recorrido inorden: "); //llamada a getInorden
 			ArrayList<String> in = exp.getInorden();
 			for (int i = 0; i<in.size(); ++i) {
 				System.out.print(in.get(i));
 				System.out.print("\n");
 			}
-			System.out.print("Recorrido postorden: ");
+			System.out.print("Recorrido postorden: "); //llamada a getPostorden
 			ArrayList<String> post = exp.getPostorden();
 			for (int i = 0; i<post.size(); ++i) {
 				System.out.print(post.get(i));
